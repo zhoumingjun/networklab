ca63abe2a1d9fb47022b5827598ebb99d9de37d4
zhoumingjun <zhoumingjun@sensetime.com>
2023-10-19 09:23:42 +0800

    update

---
+++ cybertraining/SDN/lab12/set_MACs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+for idx in {1..4}
+do
+
+PID=`lsns --type net | grep "mininet:h"${idx} | awk '{print $4}'`
+SUBMAC=`printf "%02d" $idx`
+nsenter -n -t ${PID} ip link set address 00:00:00:00:00:${SUBMAC} dev h${idx}-eth0
+echo h${idx} information
+nsenter -n -t ${PID} ip address show h${idx}-eth0 
+
+done
+
+
+
+
